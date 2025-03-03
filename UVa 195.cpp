d12435c9f7cd499b4397b4bf3c2e4fcaeb9c3f4b
MD. SAMIUL ALIM <samiulpust25@gmail.com>
2025-03-03 21:09:40 +0600

    Add files via upload

---
+++ UVa 195.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+bool cmp(char a, char b)
+{
+    if(tolower(a) == tolower(b))
+        return a<b;
+    return tolower(a)<tolower(b);
+}
+int main()
+{
+    char word[1000];
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        cin.ignore();
+        cin >> word;
+        sort(word,word+strlen(word),cmp);
+        cout << word << endl;
+        while(next_permutation(word,word+strlen(word),cmp))
+            cout<<word<<endl;
+    }
+    return 0;
+}
