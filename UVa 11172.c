b2dcce3ed5d464d5ac24168d2a97312329d495d4
MD. SAMIUL ALIM <samiulpust25@gmail.com>
2025-04-02 10:23:32 +0600

    Add files via upload

---
+++ UVa 11172.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int a, b, c, i;
+  scanf("%d", & a);
+  while (a == 1) {
+    for (i = 0; i < a; i++) {
+      scanf("%d%d", & b, & c);
+      if (b > c)
+        printf(">\n");
+      else if (b < c)
+        printf("<\n");
+      else
+        printf("=\n");
+    }
+  }
+  return 0;
+}
